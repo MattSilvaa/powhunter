99871c59c4b3633f2cdfdd6e9929d5427269ed02
MattSilvaa <mattsilvaa@gmail.com>
2025-06-20 10:36:32 -0700

    update proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,4 +3,13 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [reactRouter()],
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_PROXY_TARGET || 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
